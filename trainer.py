20eed6a6aaea4ba7895afdaf0ebaaf9bda01084a
MingshanHe <hemingshan_1999@163.com>
2022-03-11 14:45:31 +0800

    update trainer.py

--- trainer.py
+++ trainer.py
@@ -1,25 +1,102 @@
 
 import numpy as np
 class NeuralNetwork():
-    def __init__(self):
-        self.force = np.zeros((1,2))
-        self.w_x = np.zeros((2,1))
-        self.w_y = np.zeros((2,1))
-        self.b = np.zeros((2,1))
+    def __init__(self, sizes):
+        """
+        sizes = [2,4,2]
+        """
 
-        self.w_x[0][0] = 0.0101
-        self.w_x[1][0] = 0.001
-        self.w_y[0][0] = 0.001
-        self.w_y[1][0] = 0.0101
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x)
+                        for x, y in zip(sizes[:-1], sizes[1:])]
+    def SGD(self, training_data, epochs, mini_batch_size, eta, test_data=None):
+        if test_data: n_test = len(test_data)
+        n = len(training_data)
+        for j in range(epochs):
+            np.random.shuffle(training_data)
+            mini_batches = [
+                training_data[k:k+mini_batch_size]
+                for k in range(0, n, mini_batch_size)
+            ]
+            for minibatch in mini_batches:
+                self.update_mini_batch(mini_batch, eta)
 
-        self.b[0][0] = -0.0101
-        self.b[1][0] = -0.0101
+            if test_data:
+                print("Epoch {0}: {1}/{2}".format(j, self.evaluate(test_data), n_test))
+            else:
+                print("Epoch {0} complete".format(j))
 
-    def forward(self, force):
-        self.force[0][0] = force[0]
-        self.force[0][1] = force[1]
+    def update_mini_batch(self, mini_batch, eta):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
 
-        pos_x = np.dot(self.force, self.w_x) + self.b[0]
-        pos_y = np.dot(self.force, self.w_y) + self.b[1]
+        for input, output in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(input, output)
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+            self.biases     = [b-(eta/len(mini_batch))*nb for b, nb in zip(self.biases, nabla_b)]
+            self.weights    = [w-(eta/len(mini_batch))*nw for w, nw in zip(self.weights, nabla_w)]
 
-        return (pos_x, pos_y)
+    def backprop(self, input, output):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+
+        activation = x
+        activations = [x]
+        zs - []
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation) + b
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+
+        delta = self.cost_derivative(activations[-1], output) * sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, np.asarray(activations[-2]).T)
+        for l in range(2, self.num_layers):
+            z = zs[-l]
+            sp = sigmoid_prime(z)
+            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+        return (nabla_b, nabla_w)
+
+    def forward(self, data):
+        try:
+            for b, w in zip(self.biases, self.weights):
+                a = self.sigmoid(np.dot(w, data)+b)
+            return a
+        except:
+            print("Layer Error.")
+        # self.force[0][0] = force[0]
+        # self.force[0][1] = force[1]
+        # if not (len(data) == self.sizes[0]):
+        #     print("")
+
+        # pos_x = np.dot(self.force, self.w_x) + self.b[0]
+        # pos_y = np.dot(self.force, self.w_y) + self.b[1]
+
+        # return (pos_x, pos_y)
+
+    def backprop(self, x, y):
+        return 1
+
+    def cost_derivative(self, output_activations, y):
+        """
+        Return the vector of partial derivatives
+        """
+        return (output_activations - y)
+
+    def evaluate(self, data):
+        results = [(np.argmax(self.forward(input)), output) for (input, output) in data]
+        return sum(int(nn_output==output) for (nn_output, output) in results)
+
+    def sigmoid(self,val):
+        """The sigmoid function."""
+        return 1.0/(1.0+np.exp(-val))
+
+    def sigmoid_prime(self,val):
+        """Derivative of the sigmoid function."""
+        return sigmoid(val)*(1-sigmoid(val))
