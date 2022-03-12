b6e2dab7d29cd6040ffb569c00c5dc3f8aada5c6
MingshanHe <hemingshan_1999@163.com>
2022-03-12 16:15:43 +0800

    update plot.py

--- plot.py
+++ plot.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-filename = r'E:\Code\exploring-pushing-grasping\logs\2022-02-11@20-29-06\data\force-sensor-data\1.force_sensor_data.txt'
+filename = r'/home/robot/explore_grasp_ws/logs/2022-03-12@15-36-58/data/force-sensor-data/foce_data.csv'
 force,torque,x = [],[],[]
 force_x = []
 force_y = []
@@ -9,14 +9,13 @@ i = 0
 with open(filename, 'r') as f:#1
     lines = f.readlines()#2
     for line in lines:#3
-        value = [float(s) for s in line.split()]#4
-        force.append([value[0], value[1]])#5
-        force_x.append(value[0])
-        force_y.append(value[1])
+        force.append([line[0], line[1]])#5
+        force_x.append(line[0])
+        force_y.append(line[1])
         # force_z.append(value[2])
-        torque.append([value[3], value[4], value[5]])
+        torque.append([line[3], line[4], line[5]])
         x.append(i)
         i+=1
 
-plt.plot(x, force)
+plt.plot(x, force_x)
 plt.show()
