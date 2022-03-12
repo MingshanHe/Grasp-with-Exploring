c6a9f11483bf0e6e536f76434370edf8b5f6c405
MingshanHe <hemingshan_1999@163.com>
2022-03-12 18:34:57 +0800

    update main.py

--- main.py
+++ main.py
@@ -12,21 +12,24 @@ from ur5e import UR5E
 def main(args):
     # --------------- Setup options ---------------
     host_ip = args.host_ip
+    use_sim = args.use_sim
+    if use_sim:
+        ur5e = UR5E(host_ip,use_simulation=True)
+    else:
+        ur5e = UR5E(host_ip,use_simulation=False)
 
-    ur5e = UR5E(host_ip)
+        time.sleep(1)
 
-    time.sleep(1)
+        ur5e.GoHome()
 
-    ur5e.GoHome()
+        ur5e.GoWork()
 
-    ur5e.GoWork()
+        ur5e.Explore()
 
-    ur5e.Explore()
-
-    while True :
-        time.sleep(0.1)  #sleep first since the robot may not have processed the command yet
-        if ur5e.is_program_running():
-            break
+        while True :
+            time.sleep(0.1)  #sleep first since the robot may not have processed the command yet
+            if ur5e.is_program_running():
+                break
 
 if __name__ == '__main__':
 
@@ -34,6 +37,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train robotic agents to learn how to plan complementary pushing and grasping actions for manipulation with deep reinforcement learning in PyTorch.')
 
     # --------------- Setup options ---------------
+    parser.add_argument('--use_sim', dest='use_sim', action='store_true', default=False,      help='run in simulation?')
     parser.add_argument('--host_ip', dest='host_ip', action='store', default='192.168.1.101', help='IP address to robot arm (UR5E)')
 
     # Run main program with specified arguments
