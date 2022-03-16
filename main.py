13769d303e586e6f230e9677179189f1fcdd79de
MingshanHe <hemingshan_1999@163.com>
2022-03-16 22:51:54 +0800

    update main.py

--- main.py
+++ main.py
@@ -13,19 +13,27 @@ def main(args):
     # --------------- Setup options ---------------
     host_ip = args.host_ip
     use_sim = args.use_sim
+    iterations = args.iterations
+    train_axis = args.train_axis
     if use_sim:
-        for i in range(100):
-            print('%s iteration: %d\n' % ('Training', i))
+        print("[SETTING PARAM INFO]: Total Training Iterations: %d" % (iterations))
+        for i in range(iterations):
+            print("[PARAM INFO]: %s iteration: %d" % ('Training', i))
             #? Initialize pick-and-place system (camera and robot)
-            ur5e = UR5E(host_ip,use_simulation=True)
+            ur5e = UR5E(host_ip,use_simulation=True, train_axis=train_axis)
+
+            # ur5e.Go((-0.276, 0.0, 0.04, np.pi/2, np.pi/2, np.pi/2))
+            # ur5e.Go((-0.276, 0.0, 0.04, np.pi/2, np.pi/3, np.pi/2))
+            # ur5e.Go((-0.276, 0.0, 0.04, np.pi/2, -np.pi/3, np.pi/2))
 
             ur5e.GoHome()
 
             ur5e.GoWork()
 
             ur5e.Explore(target_pose=[-0.724, 0.0, 0.04, np.pi/2, 0.0, np.pi/2], vel=0.005)
+            print("--------------------------------------------------")
     else:
-        ur5e = UR5E(host_ip,use_simulation=False)
+        ur5e = UR5E(host_ip,use_simulation=False, train_axis=train_axis)
 
         time.sleep(1)
 
@@ -46,8 +54,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train robotic agents to learn how to plan complementary pushing and grasping actions for manipulation with deep reinforcement learning in PyTorch.')
 
     # --------------- Setup options ---------------
-    parser.add_argument('--use_sim', dest='use_sim', action='store_true', default=False,      help='run in simulation?')
-    parser.add_argument('--host_ip', dest='host_ip', action='store', default='192.168.1.101', help='IP address to robot arm (UR5E)')
+    parser.add_argument('--use_sim',    dest='use_sim',                 action='store_true', default=False,         help='run in simulation?')
+    parser.add_argument('--train_axis', dest='train_axis',              action='store', default='x y',              help='Axis to train')
+    parser.add_argument('--iterations', dest='iterations', type=int,    action='store', default=50,                 help='Iterations to train')
+
+    parser.add_argument('--host_ip',    dest='host_ip',                 action='store', default='192.168.1.101',    help='IP address to robot arm (UR5E)')
 
     # Run main program with specified arguments
     args = parser.parse_args()
