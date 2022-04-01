1e908ac530e5b91316f37cefe9a9c1f98c6980bb
MingshanHe <hemingshan_1999@163.com>
2022-04-01 14:43:03 +0800

    update model.py

--- model.py
+++ model.py
@@ -11,6 +11,8 @@ from torch.autograd import Variable
 import torchvision
 import matplotlib.pyplot as plt
 import time
+import pandas as pd
+
 
 class reinforcement_net(nn.Module):
     def __init__(self, use_cuda):
@@ -139,3 +141,54 @@ class reinforcement_net(nn.Module):
             self.output_prob.append([nn.Upsample(scale_factor=16, mode='bilinear').forward(F.grid_sample(self.graspnet(interm_grasp_feat), flow_grid_after, mode='nearest'))])
 
             return self.output_prob, self.interm_feat
+
+
+class QLearningTable:
+    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
+        self.actions = actions
+        self.lr = learning_rate
+        self.gamma = reward_decay
+        self.epsilon = e_greedy
+        self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float64)
+
+    def choose_action(self, observation):
+        self.check_state_exist(observation)
+        # action selection
+        if np.random.uniform() < self.epsilon:
+
+            state_action = self.q_table.loc[observation, :]
+
+            action = np.random.choice(state_action[state_action == np.max(state_action)].index)
+
+        else:
+
+            action = np.random.choice(self.actions)
+
+        return action
+
+    def learn(self, s, a, r, s_):
+        self.check_state_exist(s_)
+
+        q_predict = self.q_table.loc[s, a]
+
+        if s_ != 'terminal':
+
+            q_target = r + self.gamma * self.q_table.loc[s_, :].max()
+
+        else:
+
+            q_target = r # next state is terminal
+
+        self.q_table.loc[s,a] += self.lr * (q_target - q_predict) # update
+
+    def check_state_exist(self, state):
+
+        if state not in self.q_table.index:
+
+            self.q_table = self.q_table.append(
+                pd.Series(
+                    [0]*len(self.actions),
+                    index = self.q_table.columns,
+                    name=state
+                )
+            )
