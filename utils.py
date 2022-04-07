fbfa3e0c9d28f16ba1e525708ab1404cd465ab68
MingshanHe <hemingshan_1999@163.com>
2022-04-07 18:06:16 +0800

    update camera model

--- utils.py
+++ utils.py
@@ -14,6 +14,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 
+
 class Logger():
 
     def __init__(self, logging_directory):
@@ -31,9 +32,9 @@ class Logger():
         if not os.path.exists(self.force_sensor_data_directory):
             os.makedirs(self.force_sensor_data_directory)
 
-        self.heatmap_image_directory = os.path.join(self.base_directory, 'image')
-        if not os.path.exists(self.heatmap_image_directory):
-            os.makedirs(self.heatmap_image_directory)
+        self.image_directory = os.path.join(self.base_directory, 'image')
+        if not os.path.exists(self.image_directory):
+            os.makedirs(self.image_directory)
 
 
     def save_force_data(self, force_data):
@@ -46,7 +47,16 @@ class Logger():
         # plt.ion()
         # plt.pause(3)
         # plt.close()
-        cv2.imwrite(os.path.join(self.heatmap_image_directory, 'heatmap.png'), heatmap)
+        cv2.imwrite(os.path.join(self.image_directory, 'heatmap.png'), heatmap)
+
+    def save_depthImg(self, depthImg):
+        cv2.imwrite(os.path.join(self.image_directory, 'depth.png'), depthImg)
+
+    def save_colorImg(self, colorImg):
+        cv2.imwrite(os.path.join(self.image_directory, 'color.png'), colorImg)
+
+    def save_depthheatImg(self, heatImg):
+        cv2.imwrite(os.path.join(self.image_directory, 'depthheat.png'), heatImg)
 
 
 class Filter():
@@ -86,3 +96,91 @@ class CrossEntropyLoss2d(nn.Module):
 
     def forward(self, inputs, targets):
         return self.nll_loss(F.log_softmax(inputs, dim=1), targets)
+
+
+
+# Get rotation matrix from euler angles
+def euler2rotm(theta):
+    R_x = np.array([[1,         0,                  0                   ],
+                    [0,         math.cos(theta[0]), -math.sin(theta[0]) ],
+                    [0,         math.sin(theta[0]), math.cos(theta[0])  ]
+                    ])
+    R_y = np.array([[math.cos(theta[1]),    0,      math.sin(theta[1])  ],
+                    [0,                     1,      0                   ],
+                    [-math.sin(theta[1]),   0,      math.cos(theta[1])  ]
+                    ])         
+    R_z = np.array([[math.cos(theta[2]),    -math.sin(theta[2]),    0],
+                    [math.sin(theta[2]),    math.cos(theta[2]),     0],
+                    [0,                     0,                      1]
+                    ])            
+    R = np.dot(R_z, np.dot( R_y, R_x ))
+    return R
+
+def get_pointcloud(color_img, depth_img, camera_intrinsics):
+
+    # Get depth image size
+    im_h = depth_img.shape[0]
+    im_w = depth_img.shape[1]
+
+    # Project depth into 3D point cloud in camera coordinates
+    pix_x,pix_y = np.meshgrid(np.linspace(0,im_w-1,im_w), np.linspace(0,im_h-1,im_h))
+    cam_pts_x = np.multiply(pix_x-camera_intrinsics[0][2],depth_img/camera_intrinsics[0][0])
+    cam_pts_y = np.multiply(pix_y-camera_intrinsics[1][2],depth_img/camera_intrinsics[1][1])
+    cam_pts_z = depth_img.copy()
+    cam_pts_x.shape = (im_h*im_w,1)
+    cam_pts_y.shape = (im_h*im_w,1)
+    cam_pts_z.shape = (im_h*im_w,1)
+
+    # Reshape image into colors for 3D point cloud
+    rgb_pts_r = color_img[:,:,0]
+    rgb_pts_g = color_img[:,:,1]
+    rgb_pts_b = color_img[:,:,2]
+    rgb_pts_r.shape = (im_h*im_w,1)
+    rgb_pts_g.shape = (im_h*im_w,1)
+    rgb_pts_b.shape = (im_h*im_w,1)
+
+    cam_pts = np.concatenate((cam_pts_x, cam_pts_y, cam_pts_z), axis=1)
+    rgb_pts = np.concatenate((rgb_pts_r, rgb_pts_g, rgb_pts_b), axis=1)
+
+    return cam_pts, rgb_pts
+
+
+def get_heightmap(color_img, depth_img, cam_intrinsics, cam_pose, workspace_limits, heightmap_resolution):
+
+    # Compute heightmap size
+    heightmap_size = np.round(((workspace_limits[1][1] - workspace_limits[1][0])/heightmap_resolution, (workspace_limits[0][1] - workspace_limits[0][0])/heightmap_resolution)).astype(int)
+
+    # Get 3D point cloud from RGB-D images
+    surface_pts, color_pts = get_pointcloud(color_img, depth_img, cam_intrinsics)
+
+    # Transform 3D point cloud from camera coordinates to robot coordinates
+    surface_pts = np.transpose(np.dot(cam_pose[0:3,0:3],np.transpose(surface_pts)) + np.tile(cam_pose[0:3,3:],(1,surface_pts.shape[0])))
+
+    # Sort surface points by z value
+    sort_z_ind = np.argsort(surface_pts[:,2])
+    surface_pts = surface_pts[sort_z_ind]
+    color_pts = color_pts[sort_z_ind]
+
+    # Filter out surface points outside heightmap boundaries
+    heightmap_valid_ind = np.logical_and(np.logical_and(np.logical_and(np.logical_and(surface_pts[:,0] >= workspace_limits[0][0], surface_pts[:,0] < workspace_limits[0][1]), surface_pts[:,1] >= workspace_limits[1][0]), surface_pts[:,1] < workspace_limits[1][1]), surface_pts[:,2] < workspace_limits[2][1])
+    surface_pts = surface_pts[heightmap_valid_ind]
+    color_pts = color_pts[heightmap_valid_ind]
+
+    # Create orthographic top-down-view RGB-D heightmaps
+    color_heightmap_r = np.zeros((heightmap_size[0], heightmap_size[1], 1), dtype=np.uint8)
+    color_heightmap_g = np.zeros((heightmap_size[0], heightmap_size[1], 1), dtype=np.uint8)
+    color_heightmap_b = np.zeros((heightmap_size[0], heightmap_size[1], 1), dtype=np.uint8)
+    depth_heightmap = np.zeros(heightmap_size)
+    heightmap_pix_x = np.floor((surface_pts[:,0] - workspace_limits[0][0])/heightmap_resolution).astype(int)
+    heightmap_pix_y = np.floor((surface_pts[:,1] - workspace_limits[1][0])/heightmap_resolution).astype(int)
+    color_heightmap_r[heightmap_pix_y,heightmap_pix_x] = color_pts[:,[0]]
+    color_heightmap_g[heightmap_pix_y,heightmap_pix_x] = color_pts[:,[1]]
+    color_heightmap_b[heightmap_pix_y,heightmap_pix_x] = color_pts[:,[2]]
+    color_heightmap = np.concatenate((color_heightmap_r, color_heightmap_g, color_heightmap_b), axis=2)
+    depth_heightmap[heightmap_pix_y,heightmap_pix_x] = surface_pts[:,2]
+    z_bottom = workspace_limits[2][0]
+    depth_heightmap = depth_heightmap - z_bottom
+    depth_heightmap[depth_heightmap < 0] = 0
+    depth_heightmap[depth_heightmap == -z_bottom] = np.nan
+
+    return color_heightmap, depth_heightmap
